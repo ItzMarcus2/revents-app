3a5bffbed58429745310d41ec3064936fd2b917c
DarkStarPvP <admin@darkstarpvp.com>
2018-07-09 18:07:13 +0200

    end of Section 10 and 11

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,29 +1,29 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from '../reducers/rootReducer';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from '../reducers/rootReducer'
 
-export const configureStore = preloadedState => {
-  const middleWares = [];
-  const middleWareEnhancer = applyMiddleware(...middleWares);
+export const configureStore = (preloadedState) => {
+  const middlewares = [];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
 
-  const storeEnhancers = [middleWareEnhancer];
+  const storeEnhancers = [middlewareEnhancer];
 
-  const composedEnchancer = composeWithDevTools(...storeEnhancers);
+  const composedEnhancer  = composeWithDevTools(...storeEnhancers);
 
   const store = createStore(
-      rootReducer,
-      preloadedState,
-      composedEnchancer
-  );
+    rootReducer,
+    preloadedState,
+    composedEnhancer
+  )
 
   if (process.env.NODE_ENV !== 'production') {
     if (module.hot) {
-      module.hot.accept('../reducers/rootReducer.js', () => {
+      module.hot.accept('../reducers/rootReducer', () => {
         const newRootReducer = require('../reducers/rootReducer').default;
-        store.replaceReducer(newRootReducer);
+        store.replaceReducer(newRootReducer)
       })
     }
   }
 
   return store;
-};
+}
